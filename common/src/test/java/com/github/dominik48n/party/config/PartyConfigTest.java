6d07f5c946886a5244ab3c7b7e3a84d28fc3c14b
Dominik <yt.dominik48n@gmail.com>
2023-04-20 18:44:46 +0200

    Create test for party config

---
+++ common/src/test/java/com/github/dominik48n/party/config/PartyConfigTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 Dominik48N
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.dominik48n.party.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PartyConfigTest {
+
+    @Test
+    public void testFromDocument() {
+        final Document document = new Document().append("request_expires", 60);
+        final PartyConfig partyConfig = PartyConfig.fromDocument(document);
+
+        assertEquals(60, partyConfig.requestExpires());
+    }
+
+    @Test
+    public void testToDocument() {
+        final PartyConfig partyConfig = new PartyConfig(30);
+        final Document document = partyConfig.toDocument();
+
+        assertEquals(30, document.getInt("request_expires", -1));
+    }
+}
