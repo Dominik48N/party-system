1da3a3e28c1e66b1d419126bd82d53e9a49201c2
Dominik <yt.dominik48n@gmail.com>
2023-05-26 21:28:21 +0200

    Fix weird compiler bug

--- common/src/main/java/com/github/dominik48n/party/redis/RedisManager.java
+++ common/src/main/java/com/github/dominik48n/party/redis/RedisManager.java
@@ -42,8 +42,7 @@ public class RedisManager extends JedisPubSub {
      * @param config The {@link RedisConfig} to use.
      */
     public RedisManager(final @NotNull RedisConfig config) {
-        final JedisPoolConfig poolConfig = new Jedi
-        public sPoolConfig();
+        final JedisPoolConfig poolConfig = new JedisPoolConfig();
         this.jedisPool = config.username().isEmpty() ?
                 new JedisPool(poolConfig, config.hostname(), config.port(), 3000, config.password()) :
                 new JedisPool(poolConfig, config.hostname(), config.port(), 3000, config.username(), config.password());
