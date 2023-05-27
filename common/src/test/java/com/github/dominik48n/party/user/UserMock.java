1e8cc1c30f5d032160138848c4b8ce908b91f5ea
Dominik <yt.dominik48n@gmail.com>
2023-05-27 19:09:36 +0200

    Create user manager test

---
+++ common/src/test/java/com/github/dominik48n/party/user/UserMock.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023 Dominik48N
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.dominik48n.party.user;
+
+import com.github.dominik48n.party.api.player.PartyPlayer;
+import java.util.Optional;
+import java.util.UUID;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class UserMock implements PartyPlayer {
+
+    private final @NotNull UUID uniqueId;
+    private final @NotNull String name;
+    private final @NotNull UserManager<UserMock> userManager;
+    private @Nullable UUID partyId;
+
+    public UserMock(
+            final @NotNull UUID uniqueId,
+            final @NotNull String name,
+            final @NotNull UserManager<UserMock> userManager
+    ) {
+        this.uniqueId = uniqueId;
+        this.name = name;
+        this.userManager = userManager;
+        this.partyId = null;
+
+        this.userManager.cachePlayer(this, this);
+    }
+
+    @Override
+    public @NotNull UUID uniqueId() {
+        return this.uniqueId;
+    }
+
+    @Override
+    public @NotNull String name() {
+        return this.name;
+    }
+
+    @Override
+    public int memberLimit() {
+        return this.userManager.memberLimit(this);
+    }
+
+    @Override
+    public @NotNull Optional<UUID> partyId() {
+        return Optional.ofNullable(this.partyId);
+    }
+
+    @Override
+    public void partyId(final @Nullable UUID partyId) {
+        this.partyId = partyId;
+    }
+
+    @Override
+    public void sendMessage(final @NotNull String messageKey, final @NotNull Object... replacements) {
+        this.userManager.sendMessage(this.uniqueId, this.userManager.messageConfig().getMessage(messageKey, replacements));
+    }
+}
