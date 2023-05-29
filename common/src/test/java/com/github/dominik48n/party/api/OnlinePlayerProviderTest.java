8d5d59212bcd7c778bb794a2517a496689e3cbd7
Dominik <yt.dominik48n@gmail.com>
2023-05-29 17:59:30 +0200

    Add test for multiple online players get

--- common/src/test/java/com/github/dominik48n/party/api/OnlinePlayerProviderTest.java
+++ common/src/test/java/com/github/dominik48n/party/api/OnlinePlayerProviderTest.java
@@ -23,7 +23,10 @@ import com.github.dominik48n.party.config.Document;
 import com.github.dominik48n.party.redis.RedisManager;
 import com.github.dominik48n.party.user.UserManager;
 import com.github.dominik48n.party.user.UserMock;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
@@ -104,6 +107,32 @@ public class OnlinePlayerProviderTest {
         when(this.jedis.get(this.playerKey)).thenReturn(null);
 
         final Optional<PartyPlayer> result = this.onlinePlayerProvider.get(this.uniqueId);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetByUniqueIds() throws JsonProcessingException {
+        final PartyPlayer partyPlayer = new UserMock(this.uniqueId, this.username, this.userManager);
+
+        final List<UUID> uniqueIds = Collections.singletonList(this.uniqueId);
+        final String[] keys = {this.playerKey};
+        final String[] jsonValues = {Document.MAPPER.writeValueAsString(partyPlayer)};
+        when(this.jedis.mget(keys)).thenReturn(List.of(jsonValues));
+
+        final Map<UUID, PartyPlayer> result = this.onlinePlayerProvider.get(uniqueIds);
+        assertEquals(1, result.size());
+        assertTrue(result.containsKey(this.uniqueId));
+        assertEquals(partyPlayer, result.get(this.uniqueId));
+    }
+
+    @Test
+    public void testGetByUniqueIdsNotFound() throws JsonProcessingException {
+        final List<UUID> uniqueIds = Collections.singletonList(uniqueId);
+        final String[] keys = {this.playerKey};
+        final String[] jsonValues = {null};
+        when(this.jedis.mget(keys)).thenReturn(List.of(jsonValues));
+
+        final Map<UUID, PartyPlayer> result = this.onlinePlayerProvider.get(uniqueIds);
         assertTrue(result.isEmpty());
     }
 }
